1f32f7b858142956431196b8ca11dd0a64acb3e4
Dominic  Salinas <domsal2002@gmail.com>
2024-08-03 15:19:53 -0400

    making main branch barebones

--- SplitScreen/src/utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def create_directories():
-    if not os.path.exists('videos'):
-        os.makedirs('videos')
-    if not os.path.exists('backgrounds'):
-        os.makedirs('backgrounds')
